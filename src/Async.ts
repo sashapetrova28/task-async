fcf7430793045cd317a5d2f5319fae9285be5610
sashapetrova28 <sasha_281200@mail.ru>
2022-04-18 19:17:40 +0500

    task-async

--- src/Async.ts
+++ src/Async.ts
@@ -14,13 +14,12 @@ export function mock(ms: number): Promise<number> {
 Перепишите функцию getData так, чтобы она выполнялась быстрее.
  */
 export function getData(): Promise<number[]> {
-    const result: number[] = [];
     return Promise.all([mock(100), mock(200), mock(300)]);
 }
 
 /*
-Исправьте функцию catchException так, чтобы блок try/catch обрабатывал
-завершенный с ошибкой Promise и возвращал текст ошибки.
+Исправьте функцию catchException так, чтобы блок try/catch обрабатывал завершенный 
+с ошибкой Promise и возвращал текст ошибки.
  */
 export async function catchException(): Promise<string | undefined> {
     return await Promise.reject(new Error('my error')).catch(
